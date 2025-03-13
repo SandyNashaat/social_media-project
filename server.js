cbe19540b2d82a0f59bd7ae231abf2966eabc6f8
SandyNashaat <149734857+SandyNashaat@users.noreply.github.com>
2025-03-13 12:52:15 +0200

    Update server.js

---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const mongoose = require('mongoose')
+require('dotenv').config();
+const connectDB = require('./config/db')
+const cookieParser = require('cookie-parser');
+const profileRoutes = require("./routes/profile");
+const postRoutes = require("./routes/profile");
+const uploadRoutes = require("./routes/profile");
+const videoRoutes = require("./routes/profile");
+const storyRoutes = require("./routes/profile");
+const friendRoutes = require("./routes/profile");
+const allpostRoutes = require("./routes/profile");
+const path = require("path");
+const cors = require('cors');
+
+const app = express();
+
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use("/api", videoRoutes); 
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+
+app.use(express.json())
+app.use(cors())
+app.use(cookieParser());
+
+
+
+// app.use( profileRoutes());
+// app.use("/api", postRoutes());
+// app.use("/api", uploadRoutes()); 
+// app.use("/api", storyRoutes()); 
+// app.use("/api", friendRoutes());
+// app.use("/api", allpostRoutes());
+
+app.use("/api", profileRoutes);
+app.use("/api", postRoutes);
+app.use("/api", uploadRoutes); 
+app.use("/api", storyRoutes); 
+app.use("/api", friendRoutes);
+app.use("/api", allpostRoutes);
+connectDB()
+
+const friendRoutes = require('./routes/friends');
+
+connectDB();
+app.use('/api/friends', friendRoutes);
+
+mongoose.connection.once('open', ()=>{
+    console.log('Connected DB......')
+    app.listen(process.env.PORT, ()=>{
+        console.log('SERVER In Runing.........')
+    })
+})
+
+mongoose.connection.on('error', (error)=>{
+    console.error(error)
+})
+
+module.exports = {app}
+
+
+
