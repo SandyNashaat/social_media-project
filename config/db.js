597eccb99bf2f9cd0875063cf768e4854214ab05
SandyNashaat <149734857+SandyNashaat@users.noreply.github.com>
2025-03-13 12:21:58 +0200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,15 +1,25 @@
 const mongoose = require('mongoose');
-
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+mongoose.connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }).then(async () => {
+    console.log('Connected to MongoDB');
+  
+    // حفظ عنصر في المخطط الذي سيتم حذفه بعد يوم
+    const tempItem = new ExpiringModel({
+      data: "سيتم حذفه بعد يوم",
+      expiresAt: new Date(Date.now() + 24 * 60 * 60 * 1000) // حذف بعد 24 ساعة
     });
-    console.log('Connected to Mongo');
-  } catch (error) {
-    console.error('MongoDB connection error:', error);
-  }
-};
-
-module.exports = connectDB;
+  
+    await tempItem.save();
+  
+  
+    // حفظ عنصر في المخطط الدائم (لن يُحذف)
+    const permanentItem = new PermanentModel({
+      data: "لن يتم حذفه"
+    });
+  
+    await permanentItem.save();
+   
+  
+  }).catch(err => console.error(err));
