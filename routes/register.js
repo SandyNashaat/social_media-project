42fc2ca61ed4021eea329d781d9bdd17f91718dc
SandyNashaat <149734857+SandyNashaat@users.noreply.github.com>
2025-03-13 12:45:16 +0200

    Update register.js

--- routes/register.js
+++ routes/register.js
@@ -1,9 +1,12 @@
 const {app} = require('./server')
 const {register} = require('../controllors/register')
 const {verify_otp} = require('../controllors/register')
-
+const {forgotpassword} =require('../controllors/register')
+const {resetpassword} =require('../controllors/register')
 // const {verifyJWT} = require('../middelware/verifyJWT')
 
 app.post('/register',register)
 app.post('/verify_otp',verify_otp)
+app.post('/forgot-password',forgotpassword)
+app.post('/reset-password/:token',resetpassword)
 //app.use(verifyJWT)
